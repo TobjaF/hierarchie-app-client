3875939aa515b4d055c94fdb6fa829bbec57be83
TobjaF <74833361+TobjaF@users.noreply.github.com>
2022-04-06 09:15:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Treenode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hierarchy: props.hierarchy
+    };
+  }
+  render() {
+    const hierarchy = this.state.hierarchy;
+    if (hierarchy.children.length === 0) {
+      return (
+        <li> {hierarchy.name}</li>
+      );
+    } else {
+      return (
+        <li> <span>{hierarchy.name}</span>
+          <ul>
+            {hierarchy.children.map((item, index) => (
+              <Treenode hierarchy={item} key={index}>
+              </Treenode>
+            ))}
+          </ul>
+        </li>
+      );
+    }
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      hierarchy: { "children": [] }//mockhierarchy
+    };
+  }
+
+  componentDidMount() {
+    //"proxy": "http://localhost:3001", //für ins package.json, dann nur "/hierarchy" als url
+    fetch("http://localhost:3001/hierarchy", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(res => {
+        try {
+          return res.json()
+        } catch (error) {
+          console.log('fehler', error)
+        }
+      })
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            hierarchy: result,
+            error: null
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, hierarchy } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <Treenode hierarchy={hierarchy}></Treenode>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/mock-data.js
@@ -0,0 +1,84 @@
+const mockhierarchy = {
+    "id": 1,
+    "name": "Lina",
+    "children": [
+        {
+            "id": 2,
+            "name": "Mara",
+            "children": [
+                {
+                    "id": 6,
+                    "name": "Peter",
+                    "children": []
+                },
+                {
+                    "id": 7,
+                    "name": "Dominik",
+                    "children": []
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Andreas",
+            "children": [
+                {
+                    "id": 8,
+                    "name": "Linda",
+                    "children": []
+                },
+                {
+                    "id": 9,
+                    "name": "Anna",
+                    "children": []
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "name": "Sophie",
+            "children": [
+                {
+                    "id": 10,
+                    "name": "Nico",
+                    "children": [
+                        {
+                            "id": 12,
+                            "name": "Lukas",
+                            "children": []
+                        },
+                        {
+                            "id": 13,
+                            "name": "Leandra",
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "name": "Sandra",
+            "children": [
+                {
+                    "id": 11,
+                    "name": "Erich",
+                    "children": [
+                        {
+                            "id": 14,
+                            "name": "Tabea",
+                            "children": []
+                        },
+                        {
+                            "id": 15,
+                            "name": "Kiro",
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+module.export = { mockhierarchy: mockhierarchy };
